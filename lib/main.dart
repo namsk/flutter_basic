becd5fab91e0203962e35af668bfaa7930976feb
Sean Nam <skyun.nam@gmail.com>
2024-12-30 17:25:26 +0900

    Update Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_basic/widget.dart';
 // entry point of the app
 void main() {
   runApp(const MyApp());
   // runApp 함수는 위젯(MyApp)을 받아서 위젯 트리의 루트로 만듭니다.
+  // runApp(const MyWidgetApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,25 +20,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const WidgetGalleryPage(title: 'Widget Gallery'),
     );
   }
 }
@@ -49,15 +36,6 @@ class MyHomePage extends StatefulWidget {
   - super.key : 부모 클래스의 생성자에 전달할 수 있는 키. 위젯의 고유 식별자로 사용됨
   - required : 필수 매개변수 표시
    */
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title; //  final : 변경 불가능한 변수
 
   @override
@@ -70,54 +48,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 0; //  State로 사용할 변수
 
   void _incrementCounter() {
+    //  floatingActionButton이 눌러지면 호출될 이벤트 핸들러
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,23 +1,84 @@
 //  1. Import the material package
 import "package:flutter/material.dart";
+//
+// void main() {
+//   //  entry point of the app
+//   runApp(const MyApp());
+// }
 
-void main() {
-  //  entry point of the app
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  //  생성자
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   //  생성자
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//       return const MaterialApp(
+//         home: Scaffold(
+//           body: Center(
+//             child: Text("Widget Gallery"),
+//           )
+//         )
+//       );
+//   }
+// }
+class MyWidgetApp extends StatelessWidget {
+  const MyWidgetApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return const MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text("Widget Gallery"),
-          )
-        )
-      );
+    return MaterialApp(
+      title: "Widget Gallery",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WidgetGalleryPage(title: "Widget Gallery"),
+    );
+  }
+}
+
+class WidgetGalleryPage extends StatefulWidget {
+  const WidgetGalleryPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<WidgetGalleryPage> createState() => _WidgetGalleryPageState();
+}
+
+class _WidgetGalleryPageState extends State<WidgetGalleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {}),
+      body: const Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,  //  Column 위젯의 MainAxis는 세로 방향
+            children: <Widget>[
+              Icon(Icons.favorite),
+              Icon(Icons.favorite),
+              Icon(Icons.favorite),
+              Text("Column 위젯", style: TextStyle(fontSize: 20))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,  //  Row 위젯의 MainAxis는 가로 방향
+            children: <Widget>[
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Text("Row 위젯", style: TextStyle(fontSize: 20)),
+            ]
+          ),
+        ],
+      ),
+    );
   }
 }
